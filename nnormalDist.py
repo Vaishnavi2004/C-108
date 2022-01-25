85f6598c870ad9c62f507684ebba05273335cb79
Vaishnavi2004 <78804893+Vaishnavi2004@users.noreply.github.com>
2022-01-25 17:51:34 +0530

    Add files via upload

---
+++ nnormalDist.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import csv
+import plotly.figure_factory as ff
+
+
+df = pd.read_csv("data.csv")
+
+fig = ff.create_distplot([df["Height(Inches)"].tolist()],["Height"], show_hist=False)
+fig.show()
+
+#fig = ff.create_distplot([df["Weight(Pounds)"].tolist()],["Weight"], show_hist=False)
+#fig.show()
+
